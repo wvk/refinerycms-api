916e8b27cf86984f3affdee3bafd3c2fe3c10cc4
Willem van Kerkhof <wvk@consolving.de>
2019-10-23 08:58:41 +0200

    Identify image by name

--- app/controllers/refinery/api/v1/images_controller.rb
+++ app/controllers/refinery/api/v1/images_controller.rb
@@ -10,10 +10,24 @@ module Refinery
         end
 
         def show
-          @image = Refinery::Image.
-                    includes(:translations).
-                    accessible_by(current_ability, :read).
-                    find(params[:id])
+          image_scope = @image = Refinery::Image.
+                      includes(:translations).
+                      accessible_by(current_ability, :read)
+
+          if params[:name]
+            images = image_scope.where(image_name: params[:name]).all
+            case images.count
+            when 1
+              @image = images.first
+            when 0
+              head :not_found and return
+            else
+              respond_with(images, status: :multiple_choices, default_template: :index) and return
+            end
+          else
+            @image = image_scope.find(params[:id])
+          end
+
           respond_with(@image)
         end
 
@@ -54,9 +68,15 @@ module Refinery
         end
 
         def destroy
-          @image = Refinery::Image.accessible_by(current_ability, :destroy).find(params[:id])
-          @image.destroy
-          respond_with(@image, status: 204)
+          if params[:name]
+            @images = Refinery::Image.accessible_by(current_ability, :destroy).where(image_name: params[:name])
+            @images.destroy_all
+            respond_with(@images, status: 204)
+          else
+            @image = Refinery::Image.accessible_by(current_ability, :destroy).find(params[:id])
+            @image.destroy
+            respond_with(@image, status: 204)
+          end
         end
 
         protected
